7b9cf9b15e52eaf54489db2bdc2d10733ab3f4ab
Jamie Nuttall <jamie@wattu.com>
2017-09-04 13:11:36 +0100

    exported functions from phev-mqtt

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import { messages, send, subscribe } from './mqtt_client'
 
-const phevMqtt = () => ({
-    send : send,
-    messages : topic => {
-        subscribe(topic)
-        return messages(topic)
-    }
-})
+export {messages, send, subscribe }
