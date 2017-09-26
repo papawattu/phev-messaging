129b0aca1c8f7a54509ff62bff05c2b867bd4ac3
Jamie Nuttall <jamie@wattu.com>
2017-09-26 10:46:27 +0100

    type'o

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import PubSubClient from './pubsub-client'
+import PubSubClient from './google-pubsub-client'
 import MessagingClient from './messaging-client'
 import MqttClient from './mqtt-client'
 
