e17e8a0f550c61c6ba801adc923b08663c3dd8b8
Jamie Nuttall <jamie@wattu.com>
2017-09-26 10:36:53 +0100

    add index

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import PubSubClient from './pubsub-client'
+import MessagingClient from './messaging-client'
+import MqttClient from './mqtt-client'
+
+export { 
+    PubSubClient, 
+    MessagingClient,
+    MqttClient,
+}
