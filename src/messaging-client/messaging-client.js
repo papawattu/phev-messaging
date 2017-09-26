78048b654a18de5aab99c52cb0074483e47368aa
Jamie Nuttall <jamie@wattu.com>
2017-09-26 15:11:17 +0100

    start & reg handler can only be called once

--- src/messaging-client/messaging-client.js
+++ src/messaging-client/messaging-client.js
@@ -1,7 +1,30 @@
-const MessagingClient = ({ messaging }) => ({
-    start: messaging.start,
-    registerHandler: messaging.registerHandler,
-    publish: messaging.publish
-})
+const MessagingClient = ({ messaging }) => {
+
+    let _handler = null
+    let _started = false
+
+    const registerHandler = handler => {
+        if(_handler === null) {
+            _handler = handler
+            messaging.registerHandler(handler)
+        } else {
+            log.warn('Can only register one handler')
+        }
+    }
+    const start = () => {
+        if(!_started) {
+            _started = true
+            return messaging.start()
+        } else {
+            log.warn('Start already called once')
+            return Promise.resolve()
+        }
+    }
+    return {
+        start: messaging.start,
+        registerHandler: handler => messaging.registerHandler,
+        publish: messaging.publish
+    }
+}
 
 export default MessagingClient
